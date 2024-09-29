166c4b8651ce328af8e04fe77d8031401cd307f7
miguel <miatorresch@unal.edu.co>
2024-09-29 18:23:10 -0500

    :sparkles:  backend: routes definition

--- backend/src/routes/itemRoutes.js
+++ backend/src/routes/itemRoutes.js
@@ -1,1 +1,11 @@
 // Definición de rutas
+
+const express = require('express');
+const { getItems, getItemsById } = require('../controllers/itemController');
+
+const router = express.Router();
+
+router.get('/items', getItems);
+router.get('/items/:id', getItemsById);
+
+module.exports = router;
