9921f5d0b1379c9e3ad880b0a640b4224440599c
miguel <miatorresch@unal.edu.co>
2024-09-29 18:08:17 -0500

    :construction: backend:  create constants

---
+++ backend/src/utils/constants.js
@@ -0,0 +1,10 @@
+const AUTHOR_SIGNATURE = {
+    "author":{
+        "name": "Miguel Andres",
+        "lastname": "Torres Chavarro"
+    }
+}
+
+const URL_BASE = 'https://api.mercadolibre.com';
+
+module.exports = {URL_BASE, AUTHOR_SIGNATURE}
