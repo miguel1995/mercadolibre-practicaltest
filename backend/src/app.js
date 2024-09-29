99c391d3d2adbdd1568dff5b9e1bece49e1424f7
miguel <miatorresch@unal.edu.co>
2024-09-29 18:23:42 -0500

    :construction: backend: main expressJs Logic

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,16 @@
 // Configuración de Express
+
+const express = require('express');
+const itemRoutes = require('./routes/itemRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api', itemRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
