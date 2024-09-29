2b7980cc2c5af289953a66f37b566683f895f59d
miguel <miatorresch@unal.edu.co>
2024-09-29 18:11:15 -0500

    :sparkles:  backend: main logic for get Items and Item By ID

--- backend/src/controllers/itemController.js
+++ backend/src/controllers/itemController.js
@@ -1,1 +1,62 @@
 // Lógica del endpoint
+
+const { fetchItems, fetchItemById, fetchProductDescriptionById } = require('../services/itemService');
+const {authorSignature, AUTHOR_SIGNATURE} = require("../utils/constants");
+const {buildItems, buildDetailItem} = require("../utils/builderUtils");
+
+
+
+
+const getItems = async (req, res) => {
+    const searchQuery = req.query.q;
+    try {
+
+        const serviceResult = await fetchItems(searchQuery);
+
+        const itemList = buildItems(serviceResult["results"]);
+
+        const result = {
+            author: AUTHOR_SIGNATURE,
+            categories: [],
+            items:itemList
+        }
+
+        res.status(200).json(result);
+
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching items' });
+    }
+};
+
+const getItemsById = async (req, res) => {
+    const itemId = req.params.id;
+    try {
+
+        const serviceResult = await fetchItemById(itemId);
+        if (serviceResult) {
+
+            const newItem = buildDetailItem(serviceResult);
+            const serviceDescriptionResult = await fetchProductDescriptionById(itemId);
+
+            newItem["sold_quantity"] = "";
+            newItem["description"] = serviceDescriptionResult["plain_text"]??"";
+
+            const result = {
+                author: AUTHOR_SIGNATURE,
+                item: newItem
+            }
+
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ message: 'Item not found' });
+        }
+
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching items' });
+    }
+};
+
+
+module.exports = { getItems, getItemsById };
